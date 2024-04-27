d42729a9bb2e814aa8da55a1ff00598a469ae144
Shivansh Tiwari <113789714+Fearman99@users.noreply.github.com>
2024-04-27 17:34:10 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,14 +1,29 @@
 const mongoose =require('mongoose');
 const mongoURI = 'mongodb://127.0.0.1:27017/?directConnection=true'
 
-async function connectToMongo() {
-    await
-    mongoose.connect(mongoURI).then(()=>{
-        console.log(`connected to mongo`);
-    }).catch((err)=>{
-        console.log(err);
-    })
+// async function connectToMongo() {
+//     await
+//     mongoose.connect(mongoURI).then(()=>{
+//         console.log(`connected to mongo`);
+//     }).catch((err)=>{
+//         console.log(err);
+//     })
         
-}
+// }
+mongoose.connect(
+    (mongoURI),
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 10000, // Increase timeout (in milliseconds)
+    },
+    (err) => {
+      if (err) {
+        console.error("MongoDB connection error:", err);
+      } else {
+        console.log("MongoDB connected successfully");
+      }
+    }
+  );
 
 module.exports = connectToMongo;
